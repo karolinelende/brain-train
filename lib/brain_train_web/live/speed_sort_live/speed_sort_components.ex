d8e1374570a17b6923cde16dee620dfa1b7e656c
Karoline Lende <karoline.lende@multiverse.io>
2022-09-30 14:42:38 +0100

    formatter

--- lib/brain_train_web/live/speed_sort_live/speed_sort_components.ex
+++ lib/brain_train_web/live/speed_sort_live/speed_sort_components.ex
@@ -3,28 +3,26 @@ defmodule BrainTrainWeb.Live.SpeedSortLive.SpeedSortComponents do
 
   def start_game(assigns) do
     ~H"""
-    <button phx-click="start_game"> Start game </button>
-
+    <button phx-click="start_game">Start game</button>
     """
   end
 
   def playing_game(assigns) do
     ~H"""
-    <div> The numbers are:
-    <%= for number <- @numbers do %>
-    <button phx-click="rank-number" phx-value-number={number}> <%= number %> </button>
-    <% end %>
+    <div>
+      The numbers are:
+      <%= for number <- @numbers do %>
+        <button phx-click="rank-number" phx-value-number={number}><%= number %></button>
+      <% end %>
     </div>
 
-    <div> The time is:
-    <%= @now %>
-    </div>
+    <div>The time is: <%= @now %></div>
     """
   end
 
   def ended_game(assigns) do
     ~H"""
-      <div> Game ended :( </div>
+    <div>Game ended :(</div>
     """
   end
 end
