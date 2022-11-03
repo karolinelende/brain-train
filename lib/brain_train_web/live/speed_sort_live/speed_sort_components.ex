30e34f968a6d6e3d27ed67db546dd04ae90d7707
Karoline Lende <karoline.lende@multiverse.io>
2022-11-03 14:13:38 +0000

    add nicer score table

--- lib/brain_train_web/live/speed_sort_live/speed_sort_components.ex
+++ lib/brain_train_web/live/speed_sort_live/speed_sort_components.ex
@@ -47,19 +47,48 @@ defmodule BrainTrainWeb.Live.SpeedSortLive.SpeedSortComponents do
       <% end %>
     </button>
 
-    <div>
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Score</th>
-        </tr>
-        <%= for score <- @scores do %>
-          <tr>
-            <td><%= score.name %></td>
-            <td><%= score.score %></td>
-          </tr>
-        <% end %>
-      </table>
+    <.score_table scores={@scores} />
+    """
+  end
+
+  defp score_table(assigns) do
+    ~H"""
+    <div class="px-4 sm:px-6 lg:px-8">
+      <div class="mt-8 flex flex-col">
+        <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+              <table class="min-w-full divide-y divide-gray-300">
+                <thead class="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                    >
+                      Name
+                    </th>
+                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                      Score
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200 bg-white">
+                  <%= for score <- @scores do %>
+                    <tr>
+                      <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                        <%= score.name %>
+                      </td>
+                      <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        <%= score.score %>
+                      </td>
+                    </tr>
+                  <% end %>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     """
   end
